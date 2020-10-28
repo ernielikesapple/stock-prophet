eef87a6e9b96c24bc7e2c9bbc27eea44a40e6f43
Ernie <jasonernie03@gmail.com>
2020-10-27 21:30:19 -0400

    init

---
+++ stockSource.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 20:15:32 2020
+
+@author: ernie
+"""
+
+# data source , sock name and current price:
+    
+    
+    
+                # company names, prices
+dataSources = { 'Google': 1598,88,    
+                'Apple': 116,60,
+                'Tesla': 424,68,
+                'AMD': 78,88,
+                'NIKE': 127,99,
+                'Starbucks' 90,05 ,
+                'boeing', 155,24,
+                'disney', 123,31,
+                'exxon',32,82,     #gas supplier
+                'astraZeneca', 52,48,    #usa vaccine suppliers , UK
+                'sanofi',47,40,    #usa vaccine suppliers       Fr
+                'moderna', 70,67,  #canada vaccine suppliers
+                'pfizer',47,40,    #canada vaccine suppliers
+                'baba', 317,14,    # china 
+                'baidu',134,89,    # china 
+                
+            
+                }
